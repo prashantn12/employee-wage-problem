006c14befe4de2437a4e2516046f0e59688c54d8
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-10 12:40:30 +0530

    Welcome to Employee Wage Computation Program

---
+++ main/java/com/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+
+public class EmployeeWageComputation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome Employee Wage Computation Problem" );
+    }
+}
